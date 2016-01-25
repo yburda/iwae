44b322abde3ddbf5102ff9d8d880db4cba35d71e
yburda-MBP <yburda@gmail.com>
2016-01-25 16:23:17 -0500

    typo

--- download_mnist.py
+++ download_mnist.py
@@ -18,6 +18,6 @@ if __name__ == '__main__':
     subdatasets = ['train', 'valid', 'test']
     for subdataset in subdatasets:
         filename = 'binarized_mnist_{}.amat'.format(subdataset)
-        url = 'http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_mnist_{}.amat'.format(subdatasets)
+        url = 'http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_mnist_{}.amat'.format(subdataset)
         local_filename = os.path.join(config.DATASETS_DIR, "BinaryMNIST", filename)
         urllib.urlretrieve(url, local_filename)
